7f0696fb0718d5113da65031f0ea3afac0148e75
Preram64 <preram64@gmail.com>
2016-11-02 12:15:03 -0500

    Add files via upload

--- NumberVerifyRunner.java
+++ NumberVerifyRunner.java
@@ -1,8 +1,9 @@
 //© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date - 
-//Class -
-//Lab  -
+//Name - Preethi Ramesh
+//Date - 11/1/16
+//Class - 4th Period
+//Lab  - Odd/Even
+
 
 import static java.lang.System.*;
 import java.util.Scanner;
@@ -12,9 +13,51 @@ public class NumberVerifyRunner
 	public static void main ( String[] args )
 	{
 		//add in input
-		System.out.println("5 is odd :: " + NumberVerify.isOdd(5));
-		System.out.println("5 is even :: " + NumberVerify.isEven(5));
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter a whole number :: ");
+		int num = keyboard.nextInt();
 		
+		System.out.println( num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println( num + " is even :: " + NumberVerify.isEven(num));
+		System.out.println();
+		
+		
+		
+		System.out.print("Enter a whole number :: ");
+		num = keyboard.nextInt();
+		
+		System.out.println( num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println( num + " is even :: " + NumberVerify.isEven(num));
+		System.out.println();
+		
+		
+		
+		
+		System.out.print("Enter a whole number :: ");
+		num = keyboard.nextInt();
+		
+		System.out.println( num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println( num + " is even :: " + NumberVerify.isEven(num));
+		System.out.println();
+		
+		
+		
+		System.out.print("Enter a whole number :: ");
+		num = keyboard.nextInt();
+	
+		System.out.println( num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println( num + " is even :: " + NumberVerify.isEven(num));
+		System.out.println();
+		
+		
+		
+	
+		System.out.print("Enter a whole number :: ");
+		num = keyboard.nextInt();
+	
+		System.out.println( num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println( num + " is even :: " + NumberVerify.isEven(num));
+		System.out.println();
 		//add in more test cases
 	}
 }
