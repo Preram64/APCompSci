1893774e6757ce9f4968c64b6a9d2bbd5001ea43
Preram64 <preram64@gmail.com>
2016-11-03 16:19:26 -0500

    Update CharacterRunner.java

--- CharacterRunner.java
+++ CharacterRunner.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date - 
 //Class -
@@ -7,20 +7,67 @@
 import static java.lang.System.*;
 import java.util.Scanner;
 
-public class CharacterRunner
+public class CharacterAnalyzer
 {
-	public static void main ( String[] args )
-	{
-		Scanner keyboard = new Scanner(System.in);
-		
-		out.print("Enter a letter :: ");
-		char letter = keyboard.next().charAt(0);
-		CharacterAnalyzer test = new CharacterAnalyzer(letter);
-		out.println(test);   //A
+	private char character;
 
-		//add more test cases
-		
-		
+	public void setChar(char c)
+	{	
+		character = c;
+	}
+
+	public char getChar()
+	{
+		return character;
+	}
+
+	public boolean isUpper( )
+	{
+		if (character >= 65 && character <= 90 )
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isLower( )
+	{
+		if (character >= 97 && character <= 122 )
+		{
+			return true;
+		}
+			return false;
+	}
+	
+	public boolean isNumber( )
+	{
+		if (character >= 48 && character <= 57)
+		{
+			return true;
+		}
+		return false;
+	}	
+
+	public int getASCII( )
+	{
+		return character;
+	}
+
+	public String toString()
+	{
+		if (isUpper() == true)
+		{
+			return"" + getChar() + "is an uppercase character. ASCII == " + getASCII() + "\n";
+		}
+		else if (isLower() == true)
+		{
+			return"" + getChar() + "is an lowercase character. ASCII == " + getASCII() + "\n";
+		}
+		else if 
+		{
+			return "" + getChar() + " is a number. ASCII == " + getASCII() + "\n";	  
+`		}
+}
 		
 		
 		
