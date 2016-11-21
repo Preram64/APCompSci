495413a3c31e41ea4e4d28bea040ac259c6fabca
Preram64 <preram64@gmail.com>
2016-11-21 12:48:07 -0600

    Update Discount.java

--- Chapter-5/Discount.java
+++ Chapter-5/Discount.java
@@ -1,20 +1,31 @@
 //© A+ Computer Science  -  www.apluscompsci.com
-//Name - 
-//Date -
-//Class -
-//Lab  -
+//Name - Preethi Ramesh 
+//Date - 11/3/16
+//Class - 4th Period 
+//Lab  - Discount 
 
 import static java.lang.System.*;
 import java.util.Scanner;
 
 public class Discount
 {
+	private static double bill;
 	//instance variables and constructors could be used, but are not really needed
 	
 	//getDiscountedBill() will return final amount of the bill
 	//			if the bill is >2000, the bill receives a 15% discount
-	public static double getDiscountedBill(double bill)
+	public static double getDiscountedBill(double amt)
 	{
-		return 0;
+		if( amt > 2000 )
+		{
+			bill = amt * .85;
+			
+		}
+		else
+		{
+			bill = amt;
+		}
+		
+		return bill;
 	}
 }
