a00b5cca69ec9cd329f8bc6789d6b66df2b6de90
Preram64 <preram64@gmail.com>
2016-11-21 12:42:43 -0600

    Add files via upload

---
+++ Chapter-5/StringRunner.java
@@ -0,0 +1,87 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Preethi Ramesh
+//Date - 
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class StringRunner
+{
+	public static void main ( String[] args )
+	{
+		Scanner keyboard = new Scanner( System.in );
+
+		out.print("Enter a word :: ");
+		String word  = keyboard.nextLine();
+		
+		StringOddOrEven test = new StringOddOrEven(word);
+		test.setString(word);
+		out.println(test);
+		out.println();
+		
+		
+		out.print("Enter a word :: ");
+		word  = keyboard.nextLine();
+
+		test.setString(word);
+		out.println(test);
+		out.println();
+		
+		
+		out.print("Enter a word :: ");
+		word  = keyboard.nextLine();
+
+		test.setString(word);
+		out.println(test);
+		out.println();
+		
+		
+		
+		out.print("Enter a word :: ");
+		word  = keyboard.nextLine();
+
+		test.setString(word);
+		out.println(test);
+		out.println();
+		
+		
+		out.print("Enter a word :: ");
+		word  = keyboard.nextLine();
+
+		test.setString(word);
+		out.println(test);
+		out.println();
+		
+		
+		
+		out.print("Enter a word :: ");
+		word  = keyboard.nextLine();
+
+		test.setString(word);
+		out.println(test);
+		out.println();
+		
+		
+		
+		out.print("Enter a word :: ");
+		word  = keyboard.nextLine();
+
+		test.setString(word);
+		out.println(test);
+		out.println();
+		
+		
+		
+		out.print("Enter a word :: ");
+		word  = keyboard.nextLine();
+
+		test.setString(word);
+		out.println(test);
+		out.println();
+		
+	
+		
+	}
+}
