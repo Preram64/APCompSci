2fa596d118b4cf29daab288242bbbf9dd4af1831
Preram64 <preram64@gmail.com>
2016-11-21 12:48:25 -0600

    Update DiscountRunner.java

--- Chapter-5/DiscountRunner.java
+++ Chapter-5/DiscountRunner.java
@@ -1,21 +1,77 @@
 //© A+ Computer Science  -  www.apluscompsci.com
-//Name - 
-//Date -
+//Name -
+//Date - 
 //Class -
 //Lab  -
 
-import static java.lang.System.*;
 import java.util.Scanner;
+import static java.lang.System.*;
+import static java.lang.Math.*;
 
-public class DiscountRunner
+public class DistanceRunner
 {
-	public static void main( String args[] )
+	public static void main( String[] args )
 	{
-		Scanner keyboard = new Scanner(System.in);
-		
-		out.print("Enter the original bill amount :: ");
-		double amt = keyboard.nextDouble();
-		
+		Scanner keyboard = new Scanner( System.in );
 
+		out.print("Enter X1 :: ");
+		int xOne = keyboard.nextInt();
+		out.print("Enter Y1 :: ");
+		int yOne = keyboard.nextInt();
+		out.print("Enter X2 :: ");
+		int xTwo = keyboard.nextInt();
+		out.print("Enter Y2 :: ");
+		int yTwo = keyboard.nextInt();
+
+		Distance test = new Distance(xOne,yOne,xTwo,yTwo);
+		test.setCoordinates(xOne, yOne, xTwo, yTwo);
+		out.println(test.determineClosest());
+		out.println();
+		
+		
+		
+		out.print("Enter X1 :: ");
+		xOne = keyboard.nextInt();
+		out.print("Enter Y1 :: ");
+		yOne = keyboard.nextInt();
+		out.print("Enter X2 :: ");
+		xTwo = keyboard.nextInt();
+		out.print("Enter Y2 :: ");
+		yTwo = keyboard.nextInt();
+
+		test.setCoordinates(xOne, yOne, xTwo, yTwo);
+		out.println(test.determineClosest());
+		out.println();
+		
+		
+		
+		out.print("Enter X1 :: ");
+		xOne = keyboard.nextInt();
+		out.print("Enter Y1 :: ");
+		yOne = keyboard.nextInt();
+		out.print("Enter X2 :: ");
+		xTwo = keyboard.nextInt();
+		out.print("Enter Y2 :: ");
+		yTwo = keyboard.nextInt();
+
+		test.setCoordinates(xOne, yOne, xTwo, yTwo);
+		out.println(test.determineClosest());
+		out.println();
+		
+		
+		out.print("Enter X1 :: ");
+		xOne = keyboard.nextInt();
+		out.print("Enter Y1 :: ");
+		yOne = keyboard.nextInt();
+		out.print("Enter X2 :: ");
+		xTwo = keyboard.nextInt();
+		out.print("Enter Y2 :: ");
+		yTwo = keyboard.nextInt();
+
+		test.setCoordinates(xOne, yOne, xTwo, yTwo);
+		out.println(test.determineClosest());
+		out.println();
+		
+		//add more test cases
 	}
 }
