21fcaa5f35844dc88a0a1145ce5a44a082f52b78
Preram64 <preram64@gmail.com>
2016-11-21 12:42:24 -0600

    Add files via upload

---
+++ Chapter-5/StringOddOrEven.java
@@ -0,0 +1,48 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Preethi Ramesh
+//Date - 
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class StringOddOrEven
+{
+	private String word;
+
+	public StringOddOrEven(String s)
+	{
+		word = s;
+	}
+
+	public void setString(String s)
+	{
+		word = s;
+	}
+
+ 	public boolean isEven()
+ 	{
+		if(word.length() % 2 == 0)
+		{
+			return true;
+		}
+		
+		return false;
+	}
+
+ 	public String toString()
+ 	{
+ 		String output="";
+ 		if (isEven( )== true)
+ 		{
+ 			output += word + " is even.";
+ 		}
+ 		
+ 		else
+ 		{
+ 			output += word + " is odd.";
+ 		}
+ 		return output;
+	}
+}
