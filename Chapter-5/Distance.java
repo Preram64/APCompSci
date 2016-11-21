64fc0b3853a25b5b04632fff166f5970bea20d10
Preram64 <preram64@gmail.com>
2016-11-21 12:49:10 -0600

    Update Distance.java

--- Chapter-5/Distance.java
+++ Chapter-5/Distance.java
@@ -12,39 +12,42 @@ public class Distance
 {
 	private int x1,y1,x2,y2;
 
-	public Distance()
-	{
-
-
-	}
-
 	public Distance(int xOne, int yOne, int xTwo, int yTwo)
 	{
-
-
+		setCoordinates(xOne,yOne,xTwo,yTwo);
 	}
 
 	public void setCoordinates(int xOne, int yOne, int xTwo, int yTwo)
 	{
-
+		x1 = xOne;
+		y1 = yOne;
+		x2 = xTwo;
+		y2 = yTwo;
 
 	}
 
-	public String determineClosest( )
+	public String determineClosest()
 	{
-		double distanceA = 0.0, distanceB = 0.0;
-		String result="";
+		double distanceA = ((x1*x1)-(y1*y1)); 
+		double distanceB = ((x2*x2)-(y2*y2));
+		String answer = "";
+		
+		if (distanceA < distanceB)
+		{
+			answer = "A is closer to (0,0)";
+		}
+		
+		else
+		{
+			answer = "B is closer to (0,0)";
+		}
+		
+		return answer;
 
-
-
-
-
-
-		return result;
 	}
 	
 	public String toString()
 	{
-		return "";
+		return determineClosest();
 	}
 }
