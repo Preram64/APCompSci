4c8dec55c90c397830813f695287b65e7412ff2f
Preram64 <preram64@gmail.com>
2016-11-22 12:21:17 -0600

    Add files via upload

---
+++ DigitAdder.java
@@ -0,0 +1,28 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*; 
+
+public class DigitAdder
+{
+   public static int sumDigits( int number )
+	{
+		int sum=0;
+		
+		// use a while loop
+		// look at the doc file for help
+		
+		// while (?????? > 0)
+		// {
+			   // int digit = number ??? 10;
+			   // now add this digit value to your sum
+			   // now strip the right most digit from number (use /10)
+		// }
+		
+		
+		return sum;
+	}
+}
