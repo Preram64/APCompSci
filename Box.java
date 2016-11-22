3cdf5cf7e7c2b8c443041511f95770db7ae2234b
Preram64 <preram64@gmail.com>
2016-11-22 12:11:49 -0600

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,60 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Box
+{
+	// instance variable
+	private String word;
+
+
+
+	// default constructor
+	// SAME NAME AS THE CLASS and no return type
+	public Box()
+	{
+		// initialize word to the empty string ""	
+	}
+
+
+
+	// initializer constructor 
+	// SAME NAME AS THE CLASS and no return type
+	// String variable s is created and receives a beginning value 
+	//    to store in our instance variable word
+	public Box(String s)
+	{
+		
+	} 
+
+
+	// modifier or setter method
+	// String variable s is created and receives a new value 
+	//    to store in our instance variable word
+	public void setWord(String s)
+	{
+				
+	} 
+
+
+	public void print( )
+	{
+		// you will need a loop here
+		// the loop should run word.length() times
+		// each time through your loop you will print the value of word
+		
+		// for (int ? = ?;   ? < ?;  ?++)
+		// {
+			   // print out your word (use println, not print)
+		// }
+		
+		
+		// after the loop add 2 blank lines
+		
+		
+	}
+}
