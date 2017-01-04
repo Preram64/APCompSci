e4ef05f18c65972cb50c5386c4cf8088226d1b03
Preram64 <preram64@gmail.com>
2017-01-04 13:07:45 -0600

    Finished

--- Chapter-7/MorseCode.java
+++ Chapter-7/MorseCode.java
@@ -1,6 +1,6 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
+//Â© A+ Computer Science  -  www.apluscompsci.com
+//Name - Preethi Ramesh
+//Date - 11/28/16
 //Class -
 //Lab  -
 
@@ -10,24 +10,60 @@ public class MorseCode
 {
 	private char letter;
 
-	public MorseCode()
-	{
-		
-	}
-
 	public MorseCode(char let)
 	{
-		
+		setChar(let);
 	}
 
 	public void setChar(char let)
 	{
-		
+		letter = let;
 	}
 
 	public String getMorseCode()
 	{
 		String morse="";
+		
+		switch (letter)
+		{
+			case 'A' : morse = ".-"; break;
+			case 'B' : morse = "-..."; break;
+			case 'C' : morse = "-.-."; break;
+			case 'D' : morse = "-.."; break;
+			case 'E' : morse = "."; break;
+			case 'F' : morse = "..-."; break;
+			case 'G' : morse = "--."; break;
+			case 'H' : morse = "...."; break;
+			case 'I' : morse = ".."; break;
+			case 'J' : morse = ".---"; break;
+			case 'K' : morse = "-.-"; break;
+			case 'L' : morse = ".-.."; break;
+			case 'M' : morse = "--"; break;
+			case 'N' : morse = "-."; break;
+			case 'O' : morse = "---"; break;
+			case 'P' : morse = ".--."; break;
+			case 'Q' : morse = "--.-"; break;
+			case 'R' : morse = "-"; break;
+			case 'U' : morse = "..-"; break;
+			case 'V' : morse = "...-"; break;
+			case 'W' : morse = ".--"; break;
+			case 'X' : morse = "-..-"; break;
+			case 'Y' : morse = "-.--"; break;
+			case 'Z' : morse = "--.."; break;
+			case '0' : morse = "-----"; break;
+			case '1' : morse = ".----"; break;
+			case '2' : morse = "..---"; break;
+			case '3' : morse = "...--"; break;
+			case '4' : morse = "....-"; break;
+			case '5' : morse = "....."; break;
+			case '6' : morse = "-...."; break;
+			case '7' : morse = "--..."; break;
+			case '8' : morse = "---.."; break;
+			case '9' : morse = "----."; break;
+			default: break;
+			
+		}
+		
 		
 		// use a switch statement to get the morse code String
 		//    for the instance variable letter
