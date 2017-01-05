2701c1cf37570368e5ffa3fa10e79de967fdacf2
Preram64 <preram64@gmail.com>
2017-01-05 08:08:04 -0600

    Finished

--- Chapter-8/LoopStatsRunner.java
+++ Chapter-8/LoopStatsRunner.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -20,7 +20,7 @@ public class LoopStatsRunner
 		out.println();
 		
 		// ***** fill in your name
-		out.println("My name is ????????  ????????????????");
+		out.println("My name is Preethi Ramesh");
 		out.println();		
 		out.println();		
 		
@@ -30,14 +30,14 @@ public class LoopStatsRunner
 		// create a LoopStats object and pass it 1, 5
 		//     this will store 1 and 5 inside the object 
 		
-		// LoopStats obj = ???  ???(1, 5);
+		LoopStats obj = new LoopStats(1, 5);
 		
 		
 		
 		// STEP 2: **************************************************
 		// print out what the toString() method of obj returns
 		
-		// out.println(obj.???());
+		out.println(obj);
 		
 		
 		
@@ -48,7 +48,7 @@ public class LoopStatsRunner
 		// for the numbers 1 2 3 4 5, the total is 1 + 2 + 3 + 4 + 5 
 		//    so the total should be 15
 		
-		// out.println("Total :  " + obj.???());
+		out.println("Total :  " + obj.getTotal());
 		
 		
 		// STEP 4: **************************************************
@@ -58,7 +58,10 @@ public class LoopStatsRunner
 		// for the numbers 1 2 3 4 5, the even numbers are 2 and 4
 		//    so the even count should be 2
 		
-		// out.println("Even count : " + obj.???());
+		out.println("Even count : " + obj.getEvenCount());
+		
+		
+		
 		
 		
 		
@@ -69,7 +72,8 @@ public class LoopStatsRunner
 		// for the numbers 1 2 3 4 5, the odd numbers are 1, 3 and 5
 		//    so the odd count should be 3
 		
-		// out.println("Odd count :   " + obj.???());
+		out.println("Odd count :   " + obj.getOddCount());
+		out.println();
 		
 		
 		
@@ -81,8 +85,19 @@ public class LoopStatsRunner
 		
 		// add test cases
 		
+		obj.setNums(2,8);
+		out.println(obj);
+		out.println("Total :  " + obj.getTotal());
+		out.println("Even count : " + obj.getEvenCount());
+		out.println("Odd count : " + obj.getOddCount());
+		out.println();
 		
-		
+		obj.setNums(5,15);
+		out.println(obj);
+		out.println("Total :  " + obj.getTotal());
+		out.println("Even count : " + obj.getEvenCount());
+		out.println("Odd count : " + obj.getOddCount());
+		out.println();
 				
 	}
 }
